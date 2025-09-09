83ccef51d4edd192f57383d1e1f89d2e81f12877
mohammad3a1eh <kinitegp@gmail.com>
2025-09-09 14:52:20 +0330

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/global.css";
+import "./styles/colors.css";
+import "./styles/fonts.css";
+import "./styles/tooltip.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useLocation,
+} from "react-router-dom";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { HomePage } from "./pages/HomePage/HomePage.jsx";
+import { Header } from "./components/Header/Header.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import { Loader } from "./components/Loader/Loader.jsx";
+
+
+
+function Layout() {
+    const location = useLocation();
+    const hideLayout = [].includes(location.pathname);
+
+    return (
+        <div className="main-div">
+            {!hideLayout && <Header />}
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+            {!hideLayout && <Footer />}
+        </div>
+    );
 }
 
-export default App
+export default function App() {
+    return (
+        <Router>
+            <Loader />
+            <Layout />
+        </Router>
+    );
+}
